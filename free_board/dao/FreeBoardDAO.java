645d8df8196885889bb7222d37c03dea5b4f7168
ojuva154 <72790517+ojuva154@users.noreply.github.com>
2020-10-26 09:13:04 +0900

    Add files via upload

---
+++ free_board/dao/FreeBoardDAO.java
@@ -0,0 +1,21 @@
+package com.oyster.free_board.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.dao.DataAccessException;
+
+import com.oyster.free_board.vo.FreeBoardVO;
+
+
+public interface FreeBoardDAO {
+	public List selectAllArticlesList() throws DataAccessException;
+	public int insertNewArticle(Map articleMap) throws DataAccessException;
+	//public void insertNewImage(Map articleMap) throws DataAccessException;
+	
+	public FreeBoardVO selectArticle(int articleNO) throws DataAccessException;
+	public void updateArticle(Map articleMap) throws DataAccessException;
+	public void deleteArticle(int articleNO) throws DataAccessException;
+	public List selectImageFileList(int articleNO) throws DataAccessException;
+	
+}
